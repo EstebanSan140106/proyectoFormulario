c8957ff16f92ba1b9e97a4f8bcb38fd15061e833
ESTUDIANTE <estebanasp8@gmail.com>
2025-05-12 09:49:57 -0500

    Second Commit

--- src/main/java/com/esteban/proyectoFormulario/Servlet.java
+++ src/main/java/com/esteban/proyectoFormulario/Servlet.java
@@ -1,4 +1,90 @@
 package com.esteban.proyectoFormulario;
 
-public class Servlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet("/registro")
+public class Servlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        //Obtenemos los atributos del formulario
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String email = req.getParameter("email");
+        String[] lenguajes=req.getParameterValues("lenguajes");
+        String[] roles = req.getParameterValues("roles");
+        String idioma = req.getParameter("idioma");
+        String pais = req.getParameter("pais");
+        boolean habilitar = req.getParameter("habilitar") != null && req.getParameter("habilitar").equals("on");
+        String secreto = req.getParameter("secreto");
+        Map<String , String> errores = new HashMap<>();
+        if (username==null||username.isBlank()) {
+            errores.put("username" , "El usuario es obligatorio");
+        }
+        if (password==null||password.isBlank()) {
+            errores.put("password" , "El password es obligatorio");
+        }
+        if (email==null|| !email.contains("@")) {
+            errores.put("email" , "El email es obligatorio y debeb tener un formato de correo.");
+        }
+        if (pais == null || pais.equals("")||pais.equals(" ")) {
+            errores.put("pais" , "El pais es requerido!");
+        }
+        if (lenguajes == null || lenguajes.length == 0) {
+            errores.put("lenguajes" , "Debe seleccionar al menos una opcion!");
+            }
+        if (roles == null || roles.length == 0) {
+            errores.put("roles" , "Debe seleccionar al menos un rol");
+        }
+        if (idioma == null) {
+            errores.put("idioma", "Debe seleccionar un idioma!");
+        }
+        if (errores.isEmpty()) {
+            try (PrintWriter out = resp.getWriter()) {
+                out.println("<!DOCTYPE html>");
+                out.println("<html>");
+                out.println("<head>");
+                out.println("<meta charset=utf-8>");
+                out.println("<tile> Resultado Formulario</tile>");
+                out.println("</head>");
+                out.println("<body>");
+                out.println("<h1>Resultados Formularios</h1>");
+                out.println("<ul>");
+                out.println("<li> Username: " + username +"</li>");
+                out.println("<li> Password: " + password +"</li>");
+                out.println("<li> Email: " + email +"</li>");
+                out.println("<li> Pais: " + pais + "</li>");
+                out.println("<li> Lenguajes: " + lenguajes + "</li>");
+                Arrays.asList(lenguajes).forEach(lenguaje -> {
+                    out.println("<li> " + lenguaje + "</li>");
+                });
+                out.println("</ul> </li>");
+                out.println("<li> Roles: <ul>");
+                Arrays.asList(roles).forEach(role -> {
+                out.println("<li> " + role + "</li>");
+});
+                out.println("</ul> </li>");
+                out.println("<li> Idioma: " + idioma + "</li>");
+                out.println("<li> Habilitado: " + habilitar + "</li>");
+                out.println("<li> Secreto: " + secreto + "</li>");
+                out.println("</ul>");
+                out.println("</body>");
+                out.println("</html>");
+
+            }
+        } else {
+            req.setAttribute("errores", errores);
+            getServletContext().getRequestDispatcher("/index.jsp").forward(req, resp);
+        }
+    }
 }
